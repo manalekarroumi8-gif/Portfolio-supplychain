8daf98aa5dc2e57d5572ee902dba2ef47f1294aa
manalekarroumi8-gif <manalekarroumi8@gmail.com>
2025-11-05 01:29:08 +0100

    Version finale : nom à gauche et fond molécules interactif

--- script.js
+++ script.js
@@ -6,29 +6,48 @@ canvas.height = window.innerHeight;
 
 let particlesArray = [];
 const numberOfParticles = 100;
+let mouse = { x: null, y: null, radius: 100 };
+
+window.addEventListener('mousemove', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
 
 class Particle {
-  constructor(x, y, directionX, directionY, size, color) {
+  constructor(x, y, directionX, directionY, size, color){
     this.x = x; this.y = y;
     this.directionX = directionX; this.directionY = directionY;
     this.size = size; this.color = color;
   }
-  draw() {
+  draw(){
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
   }
-  update() {
+  update(){
+    // rebond sur les bords
     if(this.x > canvas.width || this.x < 0) this.directionX = -this.directionX;
     if(this.y > canvas.height || this.y < 0) this.directionY = -this.directionY;
+
+    // réaction au curseur
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx*dx + dy*dy);
+    if(distance < mouse.radius){
+      let angle = Math.atan2(dy, dx);
+      let force = (mouse.radius - distance)/mouse.radius;
+      this.x -= Math.cos(angle) * force * 2;
+      this.y -= Math.sin(angle) * force * 2;
+    }
+
     this.x += this.directionX;
     this.y += this.directionY;
     this.draw();
   }
 }
 
-function init() {
+function init(){
   particlesArray = [];
   for(let i=0;i<numberOfParticles;i++){
     let size = Math.random()*3 + 1;
@@ -41,7 +60,7 @@ function init() {
   }
 }
 
-function connect() {
+function connect(){
   for(let a=0;a<particlesArray.length;a++){
     for(let b=a;b<particlesArray.length;b++){
       let dx = particlesArray[a].x - particlesArray[b].x;
@@ -59,7 +78,7 @@ function connect() {
   }
 }
 
-function animate() {
+function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0,0,canvas.width,canvas.height);
   particlesArray.forEach(p => p.update());
